5ecba20060aa826656c9c08cce51209a3f5841f3
FSPinho <felipe76857685@gmail.com>
2019-06-02 08:49:52 -0300

    Removing prod file

--- public/js/app.prod.js
+++
@@ -1,8 +0,0 @@
-window.firebaseConfig = {
-    apiKey: "AIzaSyD60dMPL3lygt7v0b8F5htoB463ZDSd8Jc",
-    authDomain: "wallpaper-c3912.firebaseapp.com",
-    databaseURL: "https://wallpaper-c3912.firebaseio.com",
-    projectId: "wallpaper-c3912",
-    storageBucket: "wallpaper-c3912.appspot.com",
-    messagingSenderId: "698271505091"
-};
